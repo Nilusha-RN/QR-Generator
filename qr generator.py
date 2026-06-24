d6a38cd1f30161ed19bc606e6843949a8dcd475f
Nilusha Rathwaththe <nilushanirmal883@gmail.com>
2026-06-24 22:55:49 +0530

    Enhance QR code generator with file name input
    Added user input for file name and folder path, and opened the saved QR code file.

--- qr generator.py
+++ qr generator.py
@@ -1,10 +1,15 @@
 import qrcode
+import os
 
 url = input("Enter Your URL : ") .strip()
-file_path = "F:\\Bussiness\\Project\\QR python\\qr.png"
+file_name = input("Enter QR file name : ").strip()
+folder_path = "F:\Bussiness\Project\QR\QR python"
+file_path = os.path.join(folder_path, file_name)
 
 qr = qrcode.QRCode()
 qr.add_data(url)
 
 img = qr.make_image()
 img.save(file_path)
+
+os.startfile(file_path)  
