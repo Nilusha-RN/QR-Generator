0de9653430f268edc37b08821bf926076b3b8104
Nilusha-RN <nilushanirmal883@gmail.com>
2026-07-08 05:46:44 +0530

    Add customizable QR code settings

--- qr generator.py
+++ qr generator.py
@@ -22,7 +22,15 @@ if not file_name.endswith(".png"):
 folder_path = "F:\Bussiness\Project\QR\QR python"
 file_path = os.path.join(folder_path, file_name)
 
-qr = qrcode.QRCode()
+version = int(input("Enter QR Version (1-40) [Recommended: 1-5]: "))
+box_size = int(input("Enter Box Size [Recommended: 8-12]: "))
+border = int(input("Enter Border Size [Recommended: 2-4]: "))
+
+qr = qrcode.QRCode(
+    version=version,
+    box_size=box_size,
+    border=border
+)
 qr.add_data(url)
 
 img = qr.make_image()
